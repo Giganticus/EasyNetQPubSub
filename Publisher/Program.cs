9bd65afa02e4be49e8d664ea27c1547b33c78db9
Christopher Booth <christopher.booth@juliusbaer.com>
2024-01-29 10:47:09 +0100

    Initial commit - sending and receiving strings

---
+++ Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using EasyNetQ;
+using EasyNetQ.DI;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        var connectionString = "host=localhost;username=guest;password=guest";
+        
+        using (var bus = RabbitHutch.CreateBus(
+                   connectionString,
+                   services => services.Register<IConventions>(
+                       c => 
+                       new MyConventions(c.Resolve<ITypeNameSerializer>()),
+                       Lifetime.Singleton))) 
+        {
+            var input = String.Empty;
+            Console.WriteLine("Enter a message. 'Quit' to quit.");
+            while ((input = Console.ReadLine()) != "Quit")
+            {
+                var exchange = bus.Advanced.ExchangeDeclare("my.existing.exchange", conf =>
+                {
+                    conf.WithType("direct");
+                });
+                IMessage message = new Message<string>(input);
+                bus.Advanced.PublishAsync(exchange, "#", false, message, CancellationToken.None);
+              
+                Console.WriteLine("Message published!");
+            }
+        }
+    }
+}
+
+public class MyConventions : Conventions
+{
+    public MyConventions(ITypeNameSerializer typeNameSerializer) : base(typeNameSerializer)
+    {
+        RpcRequestExchangeNamingConvention = x => "my.existing.exchange";
+        RpcResponseExchangeNamingConvention = x => "my.existing.exchange";
+        ExchangeNamingConvention = x => "my.existing.exchange";
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,39 @@
+﻿using EasyNetQ;
+using EasyNetQ.DI;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        var connectionString = "host=localhost;username=guest;password=guest";
+        
+        using (var bus = RabbitHutch.CreateBus(
+                   connectionString,
+                   services => services.Register<IConventions>(c => 
+                       new MyConventions(c.Resolve<ITypeNameSerializer>()))))
+        {
+            bus.PubSub.Subscribe<string>("test", HandleTextMessage, config =>
+            {
+                config.WithExchangeType("direct");
+            }, CancellationToken.None);
+         
+            Console.WriteLine("Listening for messages. Hit <return> to quit.");
+            Console.ReadLine();
+        }
+    }
+
+    static void HandleTextMessage(string textMessage) 
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Got message: {0}", textMessage);
+        Console.ResetColor();
+    }
+}
+
+public class MyConventions : Conventions
+{
+    public MyConventions(ITypeNameSerializer typeNameSerializer) : base(typeNameSerializer)
+    {
+        ExchangeNamingConvention = x => "my.existing.exchange";
+    }
+}
