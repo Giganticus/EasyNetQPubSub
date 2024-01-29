c0cde2d5c1324678a368e05acc6de0c37e6c2203
Christopher Booth <christopher.booth@juliusbaer.com>
2024-01-29 17:06:48 +0100

    Accepting messages without specifying the type (properties.type = "System.String, System.Private.CoreLib"

--- Subscriber/Program.cs
+++ Subscriber/Program.cs
@@ -1,4 +1,5 @@
-﻿using EasyNetQ;
+﻿using System.Text;
+using EasyNetQ;
 using EasyNetQ.DI;
 
 class Program 
@@ -9,8 +10,16 @@ class Program
         
         using (var bus = RabbitHutch.CreateBus(
                    connectionString,
-                   services => services.Register<IConventions>(c => 
-                       new MyConventions(c.Resolve<ITypeNameSerializer>()))))
+                   services =>
+                   {
+                       services.Register<IConventions>(c =>
+                           new MyConventions(c.Resolve<ITypeNameSerializer>()));
+
+                       services.Register<ITypeNameSerializer>(c => new MyTypeNameSerializer());
+
+                       services.Register<IMessageSerializationStrategy>(
+                           c => new MyMessageSerializationStrategy());
+                   }))
         {
             bus.PubSub.Subscribe<string>("test", HandleTextMessage, config =>
             {
@@ -35,5 +44,51 @@ public class MyConventions : Conventions
     public MyConventions(ITypeNameSerializer typeNameSerializer) : base(typeNameSerializer)
     {
         ExchangeNamingConvention = x => "my.existing.exchange";
+    }
+}
+
+public class MyTypeNameSerializer : ITypeNameSerializer
+{
+    public string Serialize(Type type)
+    {
+        return type.ToString();
+    }
+
+    public Type DeSerialize(string typeName)
+    {
+        return typeof(string);
+    }
+}
+
+public class MyMessageSerializationStrategy : IMessageSerializationStrategy
+{
+    private readonly ITypeNameSerializer _typeNameSerializer;
+    private readonly ISerializer _serializer;
+
+    public MyMessageSerializationStrategy(
+        ITypeNameSerializer typeNameSerializer,
+        ISerializer serializer)
+    {
+        _typeNameSerializer = typeNameSerializer;
+        _serializer = serializer;
+    }
+    
+    public SerializedMessage SerializeMessage(IMessage message)
+    {
+        var stringType = typeof(string);
+        
+        var messageBody = _serializer.MessageToBytes(stringType, message.GetBody());
+
+        var messageProperties = message.Properties;
+        messageProperties.Type = _typeNameSerializer.Serialize(stringType);
+
+        return new SerializedMessage(messageProperties, messageBody);
+    }
+
+    public IMessage DeserializeMessage(MessageProperties properties, in ReadOnlyMemory<byte> body)
+    {
+        var messageType = typeof(string);
+        var messageBody = Encoding.UTF8.GetString(body.ToArray());
+        return MessageFactory.CreateInstance(messageType, messageBody, properties);
     }
 }
